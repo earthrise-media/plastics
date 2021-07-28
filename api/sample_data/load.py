3a0e48de3f5a92341a2fc680633e379eecf40669
Tom MacWright <tom@macwright.org>
2021-07-28 17:02:59 -0400

    Remove limit

--- api/sample_data/load.py
+++ api/sample_data/load.py
@@ -7,7 +7,7 @@ from shapely.geometry import mapping, shape
 auth = ("admin", "plastics")
 headers = {"Content-type": "application/json", "Accept": "application/json"}
 API = "https://plastic-api-dorvc455lq-uc.a.run.app"
-source_data = '../../data/model_outputs/site_contours/indonesia_v0_contours_model_spectrogram_v0.0.11_2021-07-13_de-duped.geojson'
+source_data = "../../data/model_outputs/site_contours/indonesia_v0_contours_model_spectrogram_v0.0.11_2021-07-13_de-duped.geojson"
 area_key = "area (km^2)"
 
 skips = []
@@ -41,38 +41,36 @@ site_map = {}
 index = json.load(open(source_data))
 
 # set to infinity after dev. just here to make testing faster.
-limit = 0
+# limit = 0
 features = sorted(index["features"], key=keyfunc)
 for name, contours in itertools.groupby(features, keyfunc):
     contours = list(filter(require_positive_area, list(contours)))
     if len(contours) == 0:
         continue
-    centroid = mapping(shape(contours[0]['geometry']).centroid)
+    centroid = mapping(shape(contours[0]["geometry"]).centroid)
     results = locator.reverse(tuple(reversed(centroid["coordinates"])))
     print(f"Nominatim: {str(results.address)}")
     print(f"Area: {contours[0]['properties'][area_key]}")
     site_map[name] = {
         "centroid": {
-            'type': 'Feature',
-            'properties': {
+            "type": "Feature",
+            "properties": {
                 # TODO: calculate area
-                'place_name': str(results.address),
-                'area': contours[0]['properties'][area_key] * 1000000
+                "place_name": str(results.address),
+                "area": contours[0]["properties"][area_key] * 1000000,
             },
-            'geometry': centroid
+            "geometry": centroid,
         },
         "contours": contours,
     }
-    if limit > 5:
-        break
-    limit = limit + 1
+    # if limit > 5:
+    #     break
+    # limit = limit + 1
 
 for name, record in site_map.items():
     print(f"Creating site for {name}")
     resp = requests.post(
-        f"{API}/sites",
-        json=feature_collection([record["centroid"]]),
-        headers=headers
+        f"{API}/sites", json=feature_collection([record["centroid"]]), headers=headers
     ).raise_for_status()
     # TODO: this API should return the created ID, but it does not
     # https://github.com/earthrise-media/plastics/issues/31
