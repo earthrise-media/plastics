ebbd2612bef1900257a066b50129eb71f2249091
Krishna Karra <krishna.karra@gmail.com>
2022-03-08 19:50:40 -0700

    Command line script to output GeoJSON from input dlkeys:

---
+++ scripts/dlkeys_to_geojson.py
@@ -0,0 +1,58 @@
+"""
+Write out footprints specified by DLKeys as GeoJSON
+Useful for visualizing which tiles remain after population filtering
+"""
+
+from argparse import ArgumentParser
+import descarteslabs as dl
+import json
+from shapely.geometry import mapping
+
+
+def read_dlkeys_file(dlkeys_file):
+    with open(dlkeys_file, 'r') as f:
+        dlkeys = [l.rstrip() for l in f]
+
+    return dlkeys
+
+
+def dlkey_to_geojson(dlkey):
+    tile = dl.scenes.DLTile.from_key(dlkey)
+    geom = mapping(tile.geometry)
+    return geom
+
+
+def build_feature(geom):
+    feature = {'type': 'Feature', 'geometry': geom}
+    
+    return feature
+
+
+def features_to_geojson(features, geojson_file):
+    fc = {'type': 'FeatureCollection', 'features': features}
+    with open(geojson_file, 'w') as f:
+        json.dump(fc, f)
+
+
+def main(args):
+    dlkeys = read_dlkeys_file(args.dlkeys_in)
+    print(f"Found {len(dlkeys)} tiles in {args.dlkeys_in}")
+
+    features = list()
+    for dlkey in dlkeys:
+        feature = build_feature(dlkey_to_geojson(dlkey))
+        features.append(feature)
+
+    features_to_geojson(features, args.geojson_out)
+    print(f"Wrote output file {args.geojson_out}")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument('dlkeys_in', type=str, help='Input dlkeys text file')
+    parser.add_argument('geojson_out', type=str, help='Output GeoJSON file')
+    
+    args = parser.parse_args()
+
+    main(args)
